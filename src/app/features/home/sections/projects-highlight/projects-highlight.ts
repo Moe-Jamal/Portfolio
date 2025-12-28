2ac4179e6c12a0227fba965965ceeeb6580d97b0
Moe-Jimmy <mohamed.jamal@webstdy.com>
2025-12-28 18:00:45 +0200

    start highlighted project section

--- src/app/features/home/sections/projects-highlight/projects-highlight.ts
+++ src/app/features/home/sections/projects-highlight/projects-highlight.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MainButton } from '@shared/components/main-button/main-button';
+import { Bulb } from '@shared/components/bulb/bulb';
 
 @Component({
   selector: 'app-projects-highlight',
   templateUrl: './projects-highlight.html',
   styleUrl: './projects-highlight.css',
+  imports: [MainButton, Bulb],
 })
 export class ProjectsHighlightComponent {}
