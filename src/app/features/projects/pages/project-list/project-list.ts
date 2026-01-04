8a203dabd223e0e66979d6a209a2f498559b2821
Moe-Jimmy <mohamed.jamal@webstdy.com>
2026-01-04 16:53:15 +0200

    added with in memory scroll - edit some style in project details

--- src/app/features/projects/pages/project-list/project-list.ts
+++ src/app/features/projects/pages/project-list/project-list.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { PageTitle } from '@shared/components/page-title/page-title';
+import { ProjectDataService } from 'src/app/core/services/project-data.service';
+import { ProjectCard } from '@shared/components/project-card/project-card';
 
 @Component({
   selector: 'app-project-list',
   templateUrl: './project-list.html',
   styleUrl: './project-list.css',
+  imports: [PageTitle, ProjectCard],
 })
-export class ProjectListComponent {}
+export class ProjectListComponent {
+  private readonly projectService = inject(ProjectDataService);
+  loading = this.projectService.isLoading();
+  projects = this.projectService.getProjects();
+
+  ngOnInit() {
+    this.projectService.loadProjects();
+  }
+}
